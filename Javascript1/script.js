89a5e24cad3af700af7856b8ee7121dc9fe76a42
stefanitrajkovska7-blip <stefanitrajkovska7@gmail.com>
2025-11-12 01:35:14 +0100

    first-hmw

--- Javascript1/script.js
+++ Javascript1/script.js
@@ -22,7 +22,7 @@ else if(temperature <= 19){
     console.log("The weather today is " + weather2 + "! Wear a jacket!");
 }
  else if(temperature < 9) {
-    onsole.log("The weather today is " + weather1 + "Wear a winter jacket!");
+    console.log("The weather today is " + weather1 + "Wear a winter jacket!");
  }
 
  console.log("----------------");
@@ -38,7 +38,7 @@ else if(temperature2 <= 19){
     console.log("The weather today is " + weather2 + " with a " + temperature2 + " degrees temperature!" + "\nWear a jacket!");
 }
  else if(temperature2 < 9) {
-    onsole.log("The weather today is " + weather1 + "! Wear a winter jacket!");
+    console.log("The weather today is " + weather1 + "! Wear a winter jacket!");
  }
 
  console.log("----------------");
@@ -54,7 +54,7 @@ if (temperature2 >= 25 || temperature3 >= 24) {
     console.log("The weather today is " + weather2 + "\nWear a jacket!");
 }
  if(temperature2 < 9) {
-    onsole.log("The weather today is " + weather1 + "! Wear a T-shirt and take a jacket just in case!");
+    console.log("The weather today is " + weather1 + "! Wear a T-shirt and take a jacket just in case!");
  }
 
   console.log("----------------");
@@ -69,3 +69,51 @@ if (temperature2 >= 25 || temperature3 >= 24) {
     console.log("The weather today is " + weather2 + "! Wear a T-shirt and take a jacket just in case!");
  }
  
+ console.log("----------------");
+let weather4 = "unstable"
+let stromy = true
+let weatherwarning = true
+
+if(stromy && weatherwarning) {
+    console.log("The weather today is " + weather1 +", " + weather2 + " and " + weather4 + ". Do not go outside!");
+    
+} else {
+    console.log("The weather today is" + weather1 + "and" + weather3 + ". Grab your jacket and go outside!")
+}
+
+console.log("----------------");
+
+let weatherwarning2 = false
+
+if(stromy && weatherwarning2) {
+    console.log("The weather today is " + weather1 +", " + weather2 + " and " + weather4 + ". Do not go outside!");
+    
+} else {
+    console.log("The weather today is " + weather1 + " and " + weather3 + ". Grab your jacket and go outside!")
+}
+console.log("----------------");
+let sun = true
+let safe = true
+let lightrain = false
+
+if(stromy && weatherwarning2) {
+    console.log("The weather today is " + weather1 +", " + weather2 + " and " + weather4 + ". Do not go outside!"); 
+} 
+if(stromy || safe) {
+    console.log("The weather today is " + weather1 + " and " + weather2 + ". Grab your umbrella and go outside!")
+}
+if(sun || safe) {
+    console.log("The weather today is " + weather1 + " and " + weather3 + ". Grab your friends and go to the park!")
+}
+
+console.log("----------------");
+
+if(stromy && weatherwarning) {
+    console.log("The weather today is " + weather1 +", " + weather2 + " and " + weather4 + ". Do not go outside!"); 
+} 
+if(lightrain && safe) {
+    console.log("The weather today is " + weather1 + " and " + weather2 + ". Grab your umbrella and go outside!")
+}
+if(stromy && weatherwarning) {
+    console.log("Call your family and stay safe!")
+}
